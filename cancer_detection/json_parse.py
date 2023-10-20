d20d84b3609aa2a56fe5e241092e381baa1578d4
Kerem Yıldız <49817710+fuchstech@users.noreply.github.com>
2023-10-20 22:24:28 +0300

    Update json_parse.py

--- cancer_detection/json_parse.py
+++ cancer_detection/json_parse.py
@@ -25,44 +25,44 @@ data2={
     "sure": "66",
     "bayilmasayisi": ""
 }
-def parse_js_form(data2):
-    data2 = json.dumps(data2,indent=4)
-    data2 = json.loads(data2)
-    print(json.loads(json.dumps(data2["complaints"])))
-    
-def parse_js_kayit(data,type="dict"):
-    js = json.dumps(data, indent=4)
-    js = json.loads(js)
-    #change str to integer
-    for value in js:
-        if value.isupper():
-            if value == "GENDER" or value == "AGE":
-                pass
-            else:
-                js[value] = int(js[value])
-    #print(js)
+class Parse:
+    def parse_js_form(self,data2):
+        data2 = json.dumps(data2,indent=4)
+        data2 = json.loads(data2)
+        
+    def parse_js_kayit(self,data,type="dict"):
+        js = json.dumps(data, indent=4)
+        js = json.loads(js)
+        #change str to integer
+        for value in js:
+            if value.isupper():
+                if value == "GENDER" or value == "AGE":
+                    pass
+                else:
+                    js[value] = int(js[value])
+        #print(js)
 
-    dataset = {'GENDER': 'F', 'AGE': 71, 'SMOKING': 1, 'YELLOW_FINGERS': 2, 'ANXIETY': 1, 'PEER_PRESSURE': 1, 'CHRONIC DISEASE': 2, 'FATIGUE ': 2, 'ALLERGY ': 1, 'WHEEZING': 2, 'ALCOHOL CONSUMING': 1, 'COUGHING': 2, 'SHORTNESS OF BREATH': 2, 'SWALLOWING DIFFICULTY': 1, 'CHEST PAIN': 1, 'LUNG_CANCER':'1' }
+        dataset = {'GENDER': 'F', 'AGE': 71, 'SMOKING': 1, 'YELLOW_FINGERS': 2, 'ANXIETY': 1, 'PEER_PRESSURE': 1, 'CHRONIC DISEASE': 2, 'FATIGUE ': 2, 'ALLERGY ': 1, 'WHEEZING': 2, 'ALCOHOL CONSUMING': 1, 'COUGHING': 2, 'SHORTNESS OF BREATH': 2, 'SWALLOWING DIFFICULTY': 1, 'CHEST PAIN': 1, 'LUNG_CANCER':'1' }
 
-    for values in dataset:
-        for valuef in js:
-            if valuef.isupper():
-                if values == valuef:
-                    #print(values,valuef)
-                    dataset[values] = js[valuef]
-    datasetj = json.dumps(dataset,indent=4)   
-    datasetj=json.loads(datasetj)
-    print(datasetj,"\n",dataset)
-    if type == "json":
-        return datasetj
-    if type == "dict":
-        return dataset
+        for values in dataset:
+            for valuef in js:
+                if valuef.isupper():
+                    if values == valuef:
+                        #print(values,valuef)
+                        dataset[values] = js[valuef]
+        datasetj = json.dumps(dataset,indent=4)   
+        datasetj=json.loads(datasetj)
+        print(datasetj,"\n",dataset)
+        if type == "json":
+            return datasetj
+        if type == "dict":
+            return dataset
 
-def save_csv(dataset):
-    result_data = pd.read_csv("result_data.csv")
-    df = pd.DataFrame(dataset,index=[0])
-    df = pd.concat([result_data,df])
-    df.to_csv("result_data.csv",index=0)
-#parse_js_form(data2)
-dataset = parse_js_kayit(data,"dict")
-save_csv(dataset)
+    def save_csv(self,dataset):
+        result_data = pd.read_csv("result_data.csv")
+        df = pd.DataFrame(dataset,index=[0])
+        df = pd.concat([result_data,df])
+        df.to_csv("result_data.csv",index=0)
+    #parse_js_form(data2)
+    dataset = parse_js_kayit(data,"dict")
+    save_csv(dataset)
